e4c1b5075c989557480cc3388147bea95c3cee70
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-16 11:51:47 -0300

    Initial Commit

---
+++ MonitorExternoTests/MonitorExternoTests.h
@@ -0,0 +1,13 @@
+//
+//  MonitorExternoTests.h
+//  MonitorExternoTests
+//
+//  Created by Rafael Brigagão Paulino on 16/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MonitorExternoTests : SenTestCase
+
+@end
---
+++ MonitorExternoTests/MonitorExternoTests.m
@@ -0,0 +1,32 @@
+//
+//  MonitorExternoTests.m
+//  MonitorExternoTests
+//
+//  Created by Rafael Brigagão Paulino on 16/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "MonitorExternoTests.h"
+
+@implementation MonitorExternoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MonitorExternoTests");
+}
+
+@end
